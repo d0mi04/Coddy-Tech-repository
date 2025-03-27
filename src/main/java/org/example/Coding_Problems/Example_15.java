96f3894bea65f9f025ffea22c835eaf22bca4062
Dominika Windows <d0mi04@windows.com>
2025-03-27 13:37:00 +0100

    [Coding Problems] 15th solution

--- src/main/java/org/example/Coding_Problems/Example_15.java
+++ src/main/java/org/example/Coding_Problems/Example_15.java
@@ -1,2 +1,16 @@
-package org.example.Coding_Problems;public class Example_15 {
+package org.example.Coding_Problems;
+
+public class Example_15 {
+    public static void main(String[] args) {
+        System.out.printf("%d", factorial(7));
+    }
+
+    public static int factorial(int a1) {
+        // Write code here
+        int result = 1;
+        for(int i = 1; i <= a1; i++) {
+            result *= i;
+        }
+        return  result;
+    }
 }
