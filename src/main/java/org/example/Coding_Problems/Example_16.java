2aa25d263bed19fe628062b4deb44c7665d19bfd
Dominika Windows <d0mi04@windows.com>
2025-03-27 13:39:56 +0100

    [Coding Problems] 16th solution

--- src/main/java/org/example/Coding_Problems/Example_16.java
+++ src/main/java/org/example/Coding_Problems/Example_16.java
@@ -1,2 +1,12 @@
-package org.example.Coding_Problems;public class Example_16 {
+package org.example.Coding_Problems;
+
+public class Example_16 {
+    public static void main(String[] args) {
+        System.out.printf("%d", numberOfDigits(100));
+    }
+
+    public static int numberOfDigits(int a1) {
+        // Write code here
+        return String.valueOf(a1).length();
+    }
 }
