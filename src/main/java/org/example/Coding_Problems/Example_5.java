e5336b754ba079a1dcadf2a36451a9ac2e21c13d
d0mi04 <61625098+d0mi04@users.noreply.github.com>
2025-03-29 21:39:56 +0100

    Update Example_5.java

--- src/main/java/org/example/Coding_Problems/Example_5.java
+++ src/main/java/org/example/Coding_Problems/Example_5.java
@@ -2,7 +2,7 @@ package org.example.Coding_Problems;
 import java.util.Scanner;
 
 public class Example_5 {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         // Write code here
 
         Scanner scanner = new Scanner(System.in);
