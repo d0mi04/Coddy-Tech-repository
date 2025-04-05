6513c6e579f3e7c2787953c9123fea57829ef34f
Domi-HP <d0mi04@github.com>
2025-04-06 00:21:01 +0200

    [Daily challenges][updated] Reversing Musical Scales and Checking for Palindromes: https://coddy.tech/challenges/reversing_musical_scales_and_checking_for_palindromes_java?daily=true

--- src/main/java/org/example/Challenges/DailyChallenges/MusicPalindromes/PalindromeChecker.java
+++ src/main/java/org/example/Challenges/DailyChallenges/MusicPalindromes/PalindromeChecker.java
@@ -7,31 +7,35 @@ public class PalindromeChecker {
     public static void main(String[] args) {
 
         String[][] scales = {
-                {"Do", "Re", "Mi", "Re", "Do"}
+                {"'do'", "'re'", "'mi'", "'fa'", "'do'"}
         };
-        System.out.printf("%s", reverseScaleAndCheckPalindrome(scales));
+        System.out.printf("%s", reverseScaleAndCheckPalindrome(scales)[0]);
     }
 
     public static String[] reverseScaleAndCheckPalindrome(String[][] scales) {
         // Write code here
-        Map<String, Integer> notes = new HashMap<>();
-        notes.put("do", 1);
-        notes.put("re", 2);
-        notes.put("mi", 3);
-        notes.put("fa", 4);
-        notes.put("sol", 5);
-        notes.put("la", 6);
-        notes.put("si", 7);
+        Map<String, String> notes = new HashMap<>();
+        notes.put("do", "1");
+        notes.put("re", "2");
+        notes.put("mi", "3");
+        notes.put("fa", "4");
+        notes.put("sol", "5");
+        notes.put("la", "6");
+        notes.put("si", "7");
 
         StringBuilder inputScale = new StringBuilder();
         for(String[] s : scales) {
             for(String singleNote : s) {
-                inputScale.append(singleNote);
+                inputScale.append(notes.get(singleNote.replace("'", "").toLowerCase()));
             }
         }
 
-        System.out.printf("%s", inputScale);
-
-        return null;
+        String original = inputScale.toString();
+        String reversed = inputScale.reverse().toString();
+        if(original.equals(reversed)) {
+            return new String[]{"Palindrome"};
+        } else {
+            return new String[]{"Not a palindrome"};
+        }
     }
 }
